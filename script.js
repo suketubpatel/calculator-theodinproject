95f87e82960533ffc2ebff2e7262828bd2c6a81b
Suketu Patel <suketu@usa.net>
2025-08-16 12:35:07 +1000

    Display functional
    Display get poppuleted when user click digit,
    click period button and font size increases
    or decreses when number of ditits in display
    is > 10.

--- script.js
+++ script.js
@@ -1,10 +1,125 @@
 // Global variables
-let num1, num2, operator;
+const btnContainer = document.querySelectorAll(".buttons");
+const display = document.querySelector(".display");
 
-console.log("Add: " + operate(2, 3, "+"));
-console.log("Sub: " + operate(2, 3, "-"));
-console.log("Mul: " + operate(2, 3, "*"));
-console.log("Div: " + operate(12, 4, "/"));
+let num1 = null,
+  num2 = null,
+  operator = null,
+  sum = null;
+
+let displayLength = 0;
+let fontSize = readDisplayFontSize();
+let newFontSize = fontSize;
+let periodIsUsed = false;
+
+// const fontSize = readDisplayFontSize();
+
+// let btnArray = [];
+// window.addEventListener("load", (event) => {
+//   btnContainer.forEach((btn) => {
+//     let id = btn.id;
+//     let value = btn.textContent;
+
+//     switch (id) {
+//       case "btn-clear":
+//         value = "ac";
+//         break;
+//       case "btn-delete":
+//         value = "delete";
+//         break;
+//       case "btn-plus-minus":
+//         value = "plus-minus";
+//         break;
+//       case "btn-divide":
+//         value = "/";
+//         break;
+//     }
+
+//     btnArray.push({
+//       id: id,
+//       value: value,
+//     });
+//   });
+// });
+// console.log(btnArray);
+
+btnContainer.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    clickPopulateDisplay(btn); // populate display
+    readNums(btn); // read nums
+  });
+});
+
+function dititPressed(btn) {
+  // if digit pressed
+  display.textContent += btn.textContent;
+  displayLength++; // increase display digit numbers
+  updateDisplayFontSize("+");
+}
+
+function periodPressed() {
+  // if period pressed
+  periodIsUsed = true; // set period used to true
+  if (display.textContent === "") {
+    display.textContent = "0.";
+  } else {
+    display.textContent += ".";
+  }
+  displayLength++; // increase display digit numbers
+  updateDisplayFontSize("+");
+}
+
+function deleteLastDigit() {
+  if (display.textContent !== "") {
+    let str = display.textContent;
+    display.textContent = str.substring(0, str.length - 1);
+
+    updateDisplayFontSize("-");
+    displayLength--; // increase display digit numbers
+  }
+}
+
+// Populate display when user click buttons
+function clickPopulateDisplay(btn) {
+  if (btn.classList.contains("digit")) {
+    // if digit pressed
+    dititPressed(btn);
+  } else if (btn.classList.contains("btn-period") && !periodIsUsed) {
+    // if period pressed
+    periodPressed();
+  } else if (btn.classList.contains("btn-delete")) {
+    // delete last digit
+    deleteLastDigit();
+  }
+}
+
+function readNums(btn) {}
+
+// read display font size while load
+function readDisplayFontSize() {
+  let getFontSize = window
+    .getComputedStyle(display, null)
+    .getPropertyValue("font-size");
+  let fontSize = parseFloat(getFontSize);
+  return fontSize;
+}
+
+// Reduce display font size as needed.
+function updateDisplayFontSize(sign) {
+  let sizeToUpdate = fontSize * 0.07;
+
+  if (displayLength > 10) {
+    if (sign === "+") {
+      // decrise font size
+      newFontSize = newFontSize - sizeToUpdate;
+      display.style.fontSize = newFontSize + "px";
+    } else if (sign === "-") {
+      // increase font size
+      newFontSize = newFontSize + sizeToUpdate;
+      display.style.fontSize = newFontSize + "px";
+    }
+  }
+}
 
 // Function to calculate numbers
 function operate(num1, num2, operator) {
