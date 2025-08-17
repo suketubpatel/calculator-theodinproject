d83f4e2754e43f90f9dd35690ad04502b2f89e4f
Suketu Patel <suketu@usa.net>
2025-08-17 13:11:51 +1000

    Mouse clicked - calculator is functional
    Calculato is function by clicking buttons On
    calculator.

--- script.js
+++ script.js
@@ -2,65 +2,62 @@
 const btnContainer = document.querySelectorAll(".buttons");
 const display = document.querySelector(".display");
 
-let num1 = null,
-  num2 = null,
+let num1 = 0,
+  num2 = 0,
+  temp = 0,
   operator = null,
-  sum = null;
+  sum = 0;
 
 let displayLength = 0;
 let fontSize = readDisplayFontSize();
 let newFontSize = fontSize;
 let periodIsUsed = false;
+let isFirstTime = true;
 
-// const fontSize = readDisplayFontSize();
+let devideErrorMessage = "Error: Can't divide by ZERO!";
 
-// let btnArray = [];
-// window.addEventListener("load", (event) => {
-//   btnContainer.forEach((btn) => {
-//     let id = btn.id;
-//     let value = btn.textContent;
+function clearAll() {
+  // Clear display and reset variables
+  (num1 = 0), (num2 = 0), (temp = 0), (operator = null), (sum = 0);
 
-//     switch (id) {
-//       case "btn-clear":
-//         value = "ac";
-//         break;
-//       case "btn-delete":
-//         value = "delete";
-//         break;
-//       case "btn-plus-minus":
-//         value = "plus-minus";
-//         break;
-//       case "btn-divide":
-//         value = "/";
-//         break;
-//     }
-
-//     btnArray.push({
-//       id: id,
-//       value: value,
-//     });
-//   });
-// });
-// console.log(btnArray);
+  displayLength = 0;
+  newFontSize = fontSize;
+  periodIsUsed = false;
+  isFirstTime = true;
+}
 
 btnContainer.forEach((btn) => {
   btn.addEventListener("click", (event) => {
     clickPopulateDisplay(btn); // populate display
-    readNums(btn); // read nums
+    readSign(btn);
+
+    console.log("Operator: " + operator);
+    console.log("temp: " + temp);
+    console.log("num1: " + num1);
+    console.log("num2: " + num2);
+    console.log("sum: " + sum);
   });
 });
 
-function dititPressed(btn) {
+function digitPressed(btn) {
   // if digit pressed
-  display.textContent += btn.textContent;
-  displayLength++; // increase display digit numbers
-  updateDisplayFontSize("+");
+  let str = display.textContent;
+
+  if (str == "/" || str == "*" || str == "+" || str == "-") {
+    display.textContent = btn.textContent;
+  } else {
+    display.textContent += btn.textContent;
+    displayLength++; // increase display digit numbers
+    updateDisplayFontSize("+");
+  }
 }
 
 function periodPressed() {
   // if period pressed
+  let str = display.textContent;
+
   periodIsUsed = true; // set period used to true
-  if (display.textContent === "") {
+  if (str === "" || str == "/" || str == "*" || str == "+" || str == "-") {
     display.textContent = "0.";
   } else {
     display.textContent += ".";
@@ -83,18 +80,80 @@ function deleteLastDigit() {
 function clickPopulateDisplay(btn) {
   if (btn.classList.contains("digit")) {
     // if digit pressed
-    dititPressed(btn);
+    digitPressed(btn);
   } else if (btn.classList.contains("btn-period") && !periodIsUsed) {
     // if period pressed
     periodPressed();
   } else if (btn.classList.contains("btn-delete")) {
     // delete last digit
     deleteLastDigit();
+  } else if (btn.classList.contains("btn-clear")) {
+    // clear all
+    clearAll();
+    display.textContent = "";
   }
+
+  temp = parseFloat(display.textContent);
+  isNaN(temp) ? (temp = 0) : (temp = temp);
+
+  console.log("temp original: " + temp);
 }
 
-function readNums(btn) {}
+function readSign(btn) {
+  if (btn.classList.contains("btn-sign")) {
+    if (isFirstTime) {
+      num1 = temp;
+      isFirstTime = false;
+    } else {
+      if (operator === null) {
+        display.textContent = temp;
+        isFirstTime = true;
+      } else {
+        num2 = temp;
+        sum = operate(num1, num2, operator);
+      }
+    }
 
+    let id = btn.id;
+    switch (id) {
+      case "btn-divide":
+        operator = "/";
+        break;
+      case "btn-multi":
+        operator = "*";
+        break;
+      case "btn-minus":
+        operator = "-";
+        break;
+      case "btn-plus":
+        operator = "+";
+        break;
+    }
+
+    periodIsUsed = false;
+
+    display.textContent = operator;
+    displayLength = 1;
+  }
+
+  if (btn.classList.contains("btn-equals")) {
+    console.log("= clicked");
+    if (isFirstTime) {
+      num1 = temp;
+      isFirstTime = false;
+    } else {
+      if (operator === null) {
+        display.textContent = temp;
+        isFirstTime = true;
+      } else {
+        num2 = temp;
+        sum = operate(num1, num2, operator);
+        display.textContent = sum;
+        clearAll();
+      }
+    }
+  }
+}
 // read display font size while load
 function readDisplayFontSize() {
   let getFontSize = window
@@ -107,10 +166,10 @@ function readDisplayFontSize() {
 // Reduce display font size as needed.
 function updateDisplayFontSize(sign) {
   let sizeToUpdate = fontSize * 0.07;
-
   if (displayLength > 10) {
     if (sign === "+") {
       // decrise font size
+      console.log("devide by zero");
       newFontSize = newFontSize - sizeToUpdate;
       display.style.fontSize = newFontSize + "px";
     } else if (sign === "-") {
@@ -122,23 +181,28 @@ function updateDisplayFontSize(sign) {
 }
 
 // Function to calculate numbers
-function operate(num1, num2, operator) {
+function operate(num01, num02, operator) {
   let result;
+  console.log("num01: " + num01);
+  console.log("num02: " + num02);
   switch (operator) {
     case "+":
-      result = add(num1, num2);
+      result = add(num01, num02);
       break;
     case "-":
-      result = subtract(num1, num2);
+      result = subtract(num01, num02);
       break;
     case "*":
-      result = multiply(num1, num2);
+      result = multiply(num01, num02);
       break;
     case "/":
-      result = divide(num1, num2);
+      result = divide(num01, num02);
       break;
     default:
   }
+
+  num1 = result;
+
   return result;
 }
 
@@ -163,6 +227,7 @@ function divide(num1, num2) {
       return num1 / num2;
     }
   } catch (error) {
-    return "Error: Can't divide by ZERO!";
+    alert(devideErrorMessage);
+    clearAll();
   }
 }
